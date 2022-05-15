da15d5758fc536cb64518186bad00ee375c74d53
Aren Elchinyn <slizen.can@gmail.com>
2022-05-15 15:37:39 +0300

    Small fix structure

--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
-@i686-elf-gcc -std=gnu99 -ffreestanding -g -c start.s -o start.o
-@i686-elf-gcc -std=gnu99 -ffreestanding -g -c kernel.c -o kernel.o
-@i686-elf-gcc -std=gnu99 -ffreestanding -g -c port.c -o port.o
-@i686-elf-gcc -ffreestanding -nostdlib -g -T linker.ld start.o port.o kernel.o -o mykernel.elf -lgcc
+@i686-elf-gcc -std=gnu99 -ffreestanding -g -c src/start.s -o bin/start.o
+@i686-elf-gcc -std=gnu99 -ffreestanding -g -c src/kernel.c -o bin/kernel.o
+@i686-elf-gcc -std=gnu99 -ffreestanding -g -c src/ports.c -o bin/ports.o
+@i686-elf-gcc -ffreestanding -nostdlib -g -T linker.ld bin/start.o bin/ports.o bin/kernel.o -o mykernel.elf -lgcc
 @qemu\qemu-system-i386 -kernel mykernel.elf -m 4
 @pause
